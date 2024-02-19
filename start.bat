95e05e1c6ca2d276efbfdb1e27292866c165685d
kaiyga <lolp9191@mail.ru>
2024-02-19 22:04:48 +0500

    | Init "Soul of Waifu Core"  repository

--- start.bat
+++ start.bat
@@ -1,1 +1,8 @@
+python -m venv env
+.\env\Scripts\activate.bat
+
+
+pip install -r requirements.txt
+
+
 python soul_of_waifu.py
---
+++ start.sh
@@ -0,0 +1,5 @@
+python -m venv env 
+source ./env/bin/activate 
+
+# pip install -r requirements.txt
+# python soul_of_waifu.py
